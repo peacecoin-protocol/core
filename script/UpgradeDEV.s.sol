3722ba0a9be7787c788abd9370ef13bae31d2fa3
Masahiro Chiba <chiba@alhena.jp>
2025-03-06 15:49:54 +0900

    feat: fix contract address, add UpgradeDEV script

--- script/UpgradeDEV.s.sol
+++ script/UpgradeDEV.s.sol
@@ -6,10 +6,10 @@ import { BaseScript } from "./Base.s.sol";
 import { Upgrades } from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
-contract Upgrade is BaseScript {
+contract UpgradeDEV is BaseScript {
     function run() public broadcast {
-        address pceTokenAddress = 0xA4807a8C34353A5EA51aF073175950Cb6248dA7E;
-        address pceCommunityTokenAddress = 0x6A73A610707C113F34D8B82498b6868e5f7FAA74;
+        address pceTokenAddress = 0x281C4F2a7c3dF3e15DD325FD9C7477B0c4a3F0FC;
+        address pceCommunityTokenAddress = 0x44Bbd400Cb0a39C80bF10256101CD588c891E272;
 
         Upgrades.upgradeProxy(pceTokenAddress, "PCETokenV2.sol:PCETokenV2", "");
         Upgrades.upgradeBeacon(pceCommunityTokenAddress, "PCECommunityTokenV2.sol:PCECommunityTokenV2");
